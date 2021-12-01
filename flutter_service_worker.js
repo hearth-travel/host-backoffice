b9c28825795bb2f7cfc04f990bb9dda062b42e6c
Leo <dev.rignaneseleo@gmail.com>
2021-12-01 20:56:01 +0200

    deploy after style sheet

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "2b2d6b7ae8b7e1d017e08a741acad38b",
-"index.html": "54895087fd100bf343b12b4749db9b99",
-"/": "54895087fd100bf343b12b4749db9b99",
-"main.dart.js": "eb4994a5953b1eeee11e2ba543bc0d9a",
+"index.html": "c556dfda05ce8bf3dbaa73440b568bfb",
+"/": "c556dfda05ce8bf3dbaa73440b568bfb",
+"main.dart.js": "368ad6559bbf61714cca65e6775239d5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/favicon.ico": "9e459b35230283f1362af3fe8cfaf128",
 "icons/Icon-192_old.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -14,13 +14,14 @@ const RESOURCES = {
 "icons/Icon-192.png": "8085f8f653c0c5074d58e37f6862a9b7",
 "icons/Icon-512.png": "538b426dcfbad76c5175a72e1c3c12df",
 "manifest.json": "b603afd30978295fcebd73950138ab69",
-"assets/AssetManifest.json": "7a6f16bfdc267003dc6d0614ba71e28b",
+"assets/AssetManifest.json": "0cae0df81d40f4ffd8240d5d7f1da663",
 "assets/NOTICES": "044fe991680d515ffa5453069d1bf7d1",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/assets/hearth.svg": "f4a1f2f7d949b073afbe802e7c1631f3",
+"assets/assets/plus.svg": "1d74d02e410c19211bcf5fda0b4d7ffa",
 "assets/assets/logo.svg": "ec9d1b83f2283db1aef7ac0358432780"
 };
 
