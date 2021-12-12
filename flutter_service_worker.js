7a4211bb6eec248a8391c036bfc0d91c7b418f73
Leo <dev.rignaneseleo@gmail.com>
2021-12-12 16:24:11 +0200

    satellite view + delete poi

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "2b2d6b7ae8b7e1d017e08a741acad38b",
-"index.html": "479e86ca18cf0833012a647dbfbd466b",
-"/": "479e86ca18cf0833012a647dbfbd466b",
-"main.dart.js": "89b3d5c88b7d73e63bc6aae628b43356",
+"index.html": "a347e43056d01970af8fe5fb23d156fe",
+"/": "a347e43056d01970af8fe5fb23d156fe",
+"main.dart.js": "b1c9f8d83e46fa9e5edbe39d8745cfe4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/favicon.ico": "9e459b35230283f1362af3fe8cfaf128",
 "icons/Icon-192_old.png": "ac9a721a12bbc803b44f645561ecb1e1",
