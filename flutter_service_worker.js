4033079e57d677e9ccb31f0507655d2101631e0e
Leonardo Rignanese <dev.rignaneseleo@gmail.com>
2022-04-20 14:33:34 +0100

    0.20220420.0+1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8b01b9fd246fe8a303e589fc8c598af5",
 "favicon.ico": "9e459b35230283f1362af3fe8cfaf128",
-"index.html": "01ceadbdf62d756afbdb92ced0a3216a",
-"/": "01ceadbdf62d756afbdb92ced0a3216a",
-"main.dart.js": "dcc75a62aebc3c20bafaebaec8da4343",
+"index.html": "40214181863d261202ed6944ba7a45aa",
+"/": "40214181863d261202ed6944ba7a45aa",
+"main.dart.js": "d7d79b10731572824e6d128cc2688dc7",
 "404.html": "0a27a4163254fc8fce870c8cc3a3f94f",
 "img/hearth.svg": "f4a1f2f7d949b073afbe802e7c1631f3",
 "img/logo.svg": "ec9d1b83f2283db1aef7ac0358432780",
